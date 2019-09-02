74e9aab9b7c5352f4264f77310017e17b391987a
Vladyslav Vladarchyk <vvladarchyk@elektronika-ua.local>
2019-09-02 16:51:07 +0300

    start react, add router and components

---
+++ src/components/exercises-list.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class ExercisesList extends Component {
+    render() {
+        return (
+            <div>
+                <p>Your are on the Exercises List component!</p>
+            </div>
+        )
+    }
+}
